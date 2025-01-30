4597b4cb63abc46a6cf1bf881d3c8bf2f92c23bc
gonicolas <nicolas.gouy@ynov.com>
2025-01-30 11:45:05 +0100

    UI Updated !

--- Assets/ScoreManager.cs
+++ Assets/ScoreManager.cs
@@ -6,9 +6,12 @@ public class ScoreManager : MonoBehaviour
     [Header("UI References")]
     [SerializeField] private TextMeshProUGUI scoreText;
     [SerializeField] private TextMeshProUGUI finalScoreText;
+    [SerializeField] private TextMeshProUGUI highScoreText;
 
     private int currentScore = 0;
+    private int highScore = 0;
     private static ScoreManager instance;
+    private const string HIGH_SCORE_KEY = "HighScore"; // Clé pour sauvegarder le meilleur score
 
     public static ScoreManager Instance
     {
@@ -21,16 +24,21 @@ public class ScoreManager : MonoBehaviour
         {
             instance = this;
         }
+        // Charger le meilleur score au démarrage
+        highScore = PlayerPrefs.GetInt(HIGH_SCORE_KEY, 0);
     }
 
     private void Start()
     {
         UpdateScoreUI();
-
-        // Cache le texte de score final au démarrage
+        // Cacher les textes de fin de partie au démarrage
         if (finalScoreText != null)
         {
             finalScoreText.gameObject.SetActive(false);
+        }
+        if (highScoreText != null)
+        {
+            highScoreText.gameObject.SetActive(false);
         }
     }
 
@@ -56,6 +64,22 @@ public class ScoreManager : MonoBehaviour
             finalScoreText.gameObject.SetActive(true);
             finalScoreText.text = $"Votre score : {currentScore}";
             finalScoreText.color = Color.red;
+        }
+
+        // Vérifier si c'est un nouveau meilleur score
+        if (currentScore > highScore)
+        {
+            highScore = currentScore;
+            PlayerPrefs.SetInt(HIGH_SCORE_KEY, highScore);
+            PlayerPrefs.Save();
+        }
+
+        // Afficher le meilleur score
+        if (highScoreText != null)
+        {
+            highScoreText.gameObject.SetActive(true);
+            highScoreText.text = $"Meilleur score : {highScore}";
+            highScoreText.color = Color.red;
         }
     }
 }
